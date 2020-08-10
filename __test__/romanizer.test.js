d39a31eb9798f5565abda5b90d34f54a4fe5f3f8
emmeem <1124782639@qq.com>
2020-08-10 16:17:52 +0800

    test

--- __test__/romanizer.test.js
+++ __test__/romanizer.test.js
@@ -2,9 +2,8 @@ import {transform} from "../src/romanizer";
 
 describe('romanizer', () => {
     it('should return X when input 10', () => {
-        console.log(transform(10));
+        
         const formatted = transform(10);
-
         expect(formatted).toEqual('X');
     });
 });
